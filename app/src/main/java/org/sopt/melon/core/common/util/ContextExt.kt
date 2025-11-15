6111a25511b8d732c1dfca41cfbf7268ab8ddf0e
seungjun <jjw5741@naver.com>
2025-11-16 08:43:44 +0900

    [add/#1] util 추가

---
+++ app/src/main/java/org/sopt/melon/core/common/util/ContextExt.kt
@@ -0,0 +1,14 @@
+package org.sopt.melon.core.common.util
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * Toast 메시지 띄우는 함수
+ */
+fun Context.showToast(
+    message: String,
+    duration: Int = Toast.LENGTH_SHORT
+) {
+    Toast.makeText(this, message, duration).show()
+}
---
+++ app/src/main/java/org/sopt/melon/core/common/util/NoRippleInteractionSource.kt
@@ -0,0 +1,18 @@
+package org.sopt.melon.core.common.util
+
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+/**
+ *  리플 효과가 없는 MutableInteractionSource 객체
+ */
+object NoRippleInteractionSource : MutableInteractionSource {
+
+    override val interactions: Flow<Interaction> = emptyFlow()
+
+    override suspend fun emit(interaction: Interaction) {}
+
+    override fun tryEmit(interaction: Interaction) = true
+}
