d0c94ba3919dcb52ddd552e2c551c38a91b10343
seungjun <jjw5741@naver.com>
2025-11-16 09:32:51 +0900

    [add/#1] Dummy Model, Mapper 추가

---
+++ app/src/main/java/org/sopt/melon/data/mapper/DummyMapper.kt
@@ -0,0 +1,8 @@
+package org.sopt.melon.data.mapper
+
+import org.sopt.melon.data.model.Dummy
+import org.sopt.melon.data.remote.dto.DummyResponseDto
+
+fun DummyResponseDto.toModel() = Dummy(
+    dummy = dummy
+)
