1e4b00ef82d3e5ed5f00cf77228aceb929f3dece
seungjun <jjw5741@naver.com>
2025-11-16 09:33:13 +0900

    [add/#1] DummyRepository 추가

---
+++ app/src/main/java/org/sopt/melon/data/repositoryimpl/DummyRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package org.sopt.melon.data.repositoryimpl
+
+import org.sopt.melon.data.mapper.toModel
+import org.sopt.melon.data.model.Dummy
+import org.sopt.melon.data.remote.datasource.DummyDataSource
+import org.sopt.melon.data.repository.DummyRepository
+import javax.inject.Inject
+
+class DummyRepositoryImpl @Inject constructor(
+    private val dummyDataSource: DummyDataSource,
+) : DummyRepository {
+    override suspend fun getDummy(): Result<Dummy> = runCatching {
+        val response = dummyDataSource.getDummy()
+        response.toModel()
+    }
+}
