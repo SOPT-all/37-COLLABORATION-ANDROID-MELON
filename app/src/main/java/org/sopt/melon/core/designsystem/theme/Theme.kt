f96ace7fab4ed84bfbd03063c016bc851566ef21
dodo <eg4rang@naver.com>
2025-11-17 11:21:38 +0900

    [feat/#4] 디자인시스템 구성

--- app/src/main/java/org/sopt/melon/core/designsystem/theme/Theme.kt
+++ app/src/main/java/org/sopt/melon/core/designsystem/theme/Theme.kt
@@ -1,59 +1,45 @@
 package org.sopt.melon.core.designsystem.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 
-private val DarkColorScheme =
-    darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
-    )
+object MelonTheme {
+    val colors: MelonColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMelonColorsProvider.current
 
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
-    )
+    val typography: MelonTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalMelonTypographyProvider.current
+}
 
 @Composable
-fun MELONTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun ProvideMelonColorsAndTypography(
+    colors: MelonColors,
+    typography: MelonTypography,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme =
-        when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
-        }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+    CompositionLocalProvider(
+        LocalMelonColorsProvider provides colors,
+        LocalMelonTypographyProvider provides typography,
         content = content,
     )
+}
+
+@Composable
+fun MelonTheme(
+    content: @Composable () -> Unit,
+) {
+    ProvideMelonColorsAndTypography(
+        colors = defaultMelonColors,
+        typography = defaultMelonTypography,
+    ) {
+        MaterialTheme(
+            content = content,
+        )
+    }
 }
