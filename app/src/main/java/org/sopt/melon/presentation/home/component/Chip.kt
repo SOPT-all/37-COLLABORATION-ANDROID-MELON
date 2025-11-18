ce9a7751ea01b69790fbc5e58208bb12ce12565b
dodo <eg4rang@naver.com>
2025-11-18 20:46:00 +0900

    [ui/#9] 칩 구현

---
+++ app/src/main/java/org/sopt/melon/presentation/home/component/Chip.kt
@@ -0,0 +1,85 @@
+package org.sopt.melon.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.melon.core.designsystem.theme.MELONTheme
+
+@Composable
+fun Chip(
+    content: String,
+    status: ChipStatus,
+    modifier: Modifier = Modifier,
+) {
+    val style = status.getStyle()
+    Text(
+        content,
+        style = MELONTheme.typography.body.r_14,
+        color = style.contentColor,
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(32.dp))
+            .background(style.backgroundColor)
+            .border(
+                width = 0.5.dp,
+                color = style.borderColor,
+                shape = RoundedCornerShape(32.dp),
+            )
+            .padding(horizontal = 20.dp, vertical = 8.dp),
+    )
+}
+
+data class ChipStyle(
+    val contentColor: Color,
+    val backgroundColor: Color,
+    val borderColor: Color,
+)
+
+enum class ChipStatus {
+    SELECTED,
+    DEFAULT;
+
+    @Composable
+    fun getStyle(): ChipStyle = when (this) {
+        DEFAULT -> ChipStyle(
+            contentColor = MELONTheme.colors.gray100,
+            backgroundColor = Color.Transparent,
+            borderColor = MELONTheme.colors.gray400,
+        )
+
+        SELECTED -> ChipStyle(
+            contentColor = MELONTheme.colors.gray100,
+            backgroundColor = MELONTheme.colors.primary,
+            borderColor = MELONTheme.colors.primary,
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun DefaultChipPreview() {
+    MELONTheme {
+        Chip(
+            content = "우하핳하",
+            status = ChipStatus.DEFAULT,
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun SelectedChipPreview() {
+    MELONTheme {
+        Chip(
+            content = "chip",
+            status = ChipStatus.SELECTED,
+        )
+    }
+}
