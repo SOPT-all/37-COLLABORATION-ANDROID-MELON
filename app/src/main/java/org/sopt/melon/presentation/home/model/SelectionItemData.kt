8b313b90c5f795b8705256ca4412e8c33c940aa8
dodo <eg4rang@naver.com>
2025-11-20 18:11:14 +0900

    [del/#9] 셀렉션 컴포넌트 삭제 - 홈 컴포넌트에 있어 만들었으나 공통컴포넌트 일부 요소여서 삭제

--- app/src/main/java/org/sopt/melon/presentation/home/model/SelectionItemData.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.melon.presentation.home.model
-
-data class SelectionItemData(
-    val imageUrl: String,
-    val label: String,
-    val songTitle: String,
-    val singer: String,
-)
