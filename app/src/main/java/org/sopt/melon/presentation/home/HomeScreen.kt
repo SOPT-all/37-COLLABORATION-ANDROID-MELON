335e3b9d9d2b717a491848eed3c04ef0d1c02f59
dodo <eg4rang@naver.com>
2025-11-21 22:03:51 +0900

    [ui/#18] 홈 스크린 구현

--- app/src/main/java/org/sopt/melon/presentation/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/melon/presentation/home/HomeScreen.kt
@@ -1,26 +1,51 @@
 package org.sopt.melon.presentation.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.collections.immutable.persistentListOf
+import org.sopt.melon.R
 import org.sopt.melon.R.string.snackbar_mixup_add_message
 import org.sopt.melon.R.string.snackbar_move_action_label
-import org.sopt.melon.core.common.util.noRippleClickable
 import org.sopt.melon.core.designsystem.component.snackbar.LocalMelonSnackbarTrigger
 import org.sopt.melon.core.designsystem.component.snackbar.MelonSnackbarActionRequest
+import org.sopt.melon.core.designsystem.theme.MELONTheme
+import org.sopt.melon.presentation.home.component.HomeBannerList
+import org.sopt.melon.presentation.home.component.HomeChartButton
+import org.sopt.melon.presentation.home.component.HomeChartTitle
+import org.sopt.melon.presentation.home.component.HomeChipList
+import org.sopt.melon.presentation.home.component.HomeEventBanner
+import org.sopt.melon.presentation.home.component.HomeNewSongGrid
+import org.sopt.melon.presentation.home.component.HomeNewSongTitle
+import org.sopt.melon.presentation.home.component.HomePopularGrid
+import org.sopt.melon.presentation.home.component.HomePreferenceSongCard
+import org.sopt.melon.presentation.home.component.HomeRecommendList
+import org.sopt.melon.presentation.home.component.HomeTitle
+import org.sopt.melon.presentation.home.component.HomeTopBar
+import org.sopt.melon.presentation.home.model.BannerData
+import org.sopt.melon.presentation.home.model.NewSongItemData
+import org.sopt.melon.presentation.home.model.PopularSongData
+import org.sopt.melon.presentation.home.model.PreferenceSongCardData
+import org.sopt.melon.presentation.home.model.RecommendSongItemData
 
 @Composable
 fun HomeRoute(
     innerPadding: PaddingValues,
     navigateToMixUp: () -> Unit,
+    viewModel: HomeViewModel = hiltViewModel(),
 ) {
     val snackbarTrigger = LocalMelonSnackbarTrigger.current
     val snackbarRequest =
@@ -30,8 +55,14 @@ fun HomeRoute(
             onClick = navigateToMixUp,
         )
 
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    val popularGridState = rememberLazyGridState()
+
     HomeScreen(
+        uiState = uiState,
         onMixUpClick = { snackbarTrigger(snackbarRequest) },
+        popularGridState = popularGridState,
         modifier =
             Modifier
                 .padding(innerPadding),
@@ -40,24 +71,300 @@ fun HomeRoute(
 
 @Composable
 private fun HomeScreen(
+    uiState: HomeUiState,
     onMixUpClick: () -> Unit,
+    popularGridState: LazyGridState,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier =
-            modifier
-                .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    LazyColumn(
+        modifier = modifier,
     ) {
-        Text(
-            "HOME",
-            color = Color.White,
+        item {
+            HomeTopBar(
+                modifier =
+                    Modifier
+                        .padding(
+                            top = 12.dp,
+                            bottom = 8.dp,
+                        ),
+            )
+
+            HomeEventBanner(
+                modifier =
+                    Modifier
+                        .padding(
+                            start = 20.dp,
+                            end = 20.dp,
+                            bottom = 20.dp,
+                        ),
+            )
+
+            HomePreferenceSongCard(
+                data = uiState.preferenceSong,
+                modifier =
+                    Modifier
+                        .padding(
+                            start = 20.dp,
+                            end = 20.dp,
+                            bottom = 28.dp,
+                        ),
+            )
+        }
+
+        item {
+            HomeTitle(
+                title = "${uiState.userName}님을 위한 추천",
+                modifier =
+                    Modifier.padding(
+                        bottom = 12.dp,
+                    ),
+            )
+
+            HomeRecommendList(
+                recommendSongList = uiState.recommendSongList,
+                modifier =
+                    Modifier
+                        .padding(
+                            bottom = 48.dp,
+                        ),
+            )
+        }
+
+        item {
+            HomeTitle(
+                title = "인기 선곡",
+                modifier =
+                    Modifier.padding(
+                        bottom = 16.dp,
+                    ),
+            )
+
+            HomePopularGrid(
+                popularSongList = uiState.popularSongList,
+                gridState = popularGridState,
+                onMixUpClick = onMixUpClick,
+                modifier =
+                    Modifier
+                        .padding(
+                            bottom = 48.dp,
+                        ),
+            )
+        }
+
+        item {
+            HomeBannerList(
+                bannerDataList = uiState.bannerList,
+                modifier =
+                    Modifier
+                        .padding(
+                            bottom = 48.dp,
+                        ),
+            )
+        }
+
+        item {
+            HomeNewSongTitle(
+                modifier =
+                    Modifier
+                        .padding(bottom = 14.dp),
+            )
+
+            HomeNewSongGrid(
+                newSongList = uiState.newSongData,
+                modifier =
+                    Modifier
+                        .padding(
+                            bottom = 48.dp,
+                        ),
+            )
+        }
+
+        item {
+            HomeChartTitle(
+                modifier = Modifier.padding(bottom = 12.dp),
+            )
+            HomeChipList(
+                chipContentList = uiState.chipContentList,
+            )
+            // TODO: 멜론 차트 그리드
+            HomeChartButton(
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp, bottom = 60.dp),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    MELONTheme {
+        Box(
             modifier =
                 Modifier
-                    .noRippleClickable(
-                        onClick = onMixUpClick,
+                    .fillMaxSize()
+                    .background(MELONTheme.colors.background),
+        ) {
+            HomeScreen(
+                uiState =
+                    HomeUiState(
+                        userName = "안드콩",
+                        preferenceSong =
+                            PreferenceSongCardData(
+                                image = R.drawable.img_preference_95,
+                                description = "이거 진짜 좋은뎅",
+                                songTitle = "LINK IT ALL",
+                                singer = "SPYAIR",
+                            ),
+                        recommendSongList =
+                            persistentListOf(
+                                RecommendSongItemData(
+                                    imageUrl = "TODO()",
+                                    description = "내가 아끼는 최애곡 모음",
+                                ),
+                                RecommendSongItemData(
+                                    imageUrl = "TODO()",
+                                    description = "내가 아끼는 최애곡 모음",
+                                ),
+                                RecommendSongItemData(
+                                    imageUrl = "TODO()",
+                                    description = "내가 아끼는 최애곡 모음",
+                                ),
+                            ),
+                        popularSongList =
+                            persistentListOf(
+                                PopularSongData(
+                                    imgUrl = "",
+                                    subtitle = "멜론DJ's Pick",
+                                    title = "1XOXZ",
+                                    artistName = "IVE (아이브)",
+                                ),
+                                PopularSongData(
+                                    imgUrl = "",
+                                    subtitle = "멜론DJ's Pick",
+                                    title = "2XOXZ",
+                                    artistName = "IVE (아이브)",
+                                ),
+                                PopularSongData(
+                                    imgUrl = "",
+                                    subtitle = "멜론DJ's Pick",
+                                    title = "3XOXZ",
+                                    artistName = "Hearts2Hearts(하츠투하츠)",
+                                ),
+                                PopularSongData(
+                                    imgUrl = "",
+                                    subtitle = "멜론DJ's Pick",
+                                    title = "4XOXZ",
+                                    artistName = "IVE (아이브)",
+                                ),
+                                PopularSongData(
+                                    imgUrl = "",
+                                    subtitle = "멜론DJ's Pick",
+                                    title = "5XOXZ",
+                                    artistName = "IVE (아이브)",
+                                ),
+                                PopularSongData(
+                                    imgUrl = "",
+                                    subtitle = "멜론DJ's Pick",
+                                    title = "6XOXZ",
+                                    artistName = "IVE (아이브)",
+                                ),
+                            ),
+                        bannerList =
+                            persistentListOf(
+                                BannerData(
+                                    title = "2025 WOODZ PREVIEW CONCERT",
+                                    image = R.drawable.ic_launcher_background,
+                                    category = "Melon Ticket",
+                                    headline = "우즈 CONCERT",
+                                    description = "멜론티켓에서 예매하기",
+                                    backgroundColor = MELONTheme.colors.bar6,
+                                ),
+                                BannerData(
+                                    title = "2025 WOODZ PREVIEW CONCERT",
+                                    image = R.drawable.ic_launcher_background,
+                                    category = "Melon Ticket",
+                                    headline = "우즈 CONCERT",
+                                    description = "멜론티켓에서 예매하기멜론티켓에서 예매하기",
+                                    backgroundColor = MELONTheme.colors.bar6,
+                                ),
+                                BannerData(
+                                    title = "2025 WOODZ PREVIEW CONCERT",
+                                    image = R.drawable.ic_launcher_background,
+                                    category = "Melon Ticket",
+                                    headline = "우즈 CONCERT",
+                                    description = "멜론티켓에서 예매하기멜론티켓에서 예매하기멜론티켓에서 예매하기멜론티켓에서 예매하기",
+                                    backgroundColor = MELONTheme.colors.bar6,
+                                ),
+                            ),
+                        newSongData =
+                            persistentListOf(
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                                NewSongItemData(
+                                    imageUrl = "TODO()",
+                                    songTitle = "Back to Life",
+                                    singer = "&TEAM",
+                                ),
+                            ),
+                        chipContentList =
+                            persistentListOf(
+                                "TOP 100",
+                                "HOT 100",
+                                "월드뮤직",
+                                "한강에서 즐기기 좋은 음악",
+                            ),
                     ),
-        )
+                onMixUpClick = {},
+                popularGridState = rememberLazyGridState(),
+            )
+        }
     }
 }
