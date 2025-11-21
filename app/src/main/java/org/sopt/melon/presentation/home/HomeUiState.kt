1eac60346d3812382d42286eca6bc8bc86270e19
dodo <eg4rang@naver.com>
2025-11-21 22:03:15 +0900

    [feat/#18] 홈 뷰모델, uiState 적용

---
+++ app/src/main/java/org/sopt/melon/presentation/home/HomeUiState.kt
@@ -0,0 +1,29 @@
+package org.sopt.melon.presentation.home
+
+import androidx.compose.runtime.Immutable
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import org.sopt.melon.R
+import org.sopt.melon.presentation.home.model.BannerData
+import org.sopt.melon.presentation.home.model.NewSongItemData
+import org.sopt.melon.presentation.home.model.PopularSongData
+import org.sopt.melon.presentation.home.model.PreferenceSongCardData
+import org.sopt.melon.presentation.home.model.RecommendSongItemData
+
+@Immutable
+data class HomeUiState(
+    val userName: String = "",
+    val preferenceSong: PreferenceSongCardData =
+        PreferenceSongCardData(
+            image = R.drawable.img_preference_95,
+            description = "",
+            songTitle = "",
+            singer = "",
+        ),
+    val recommendSongList: ImmutableList<RecommendSongItemData> = persistentListOf(),
+    val popularSongList: ImmutableList<PopularSongData> = persistentListOf(),
+    val bannerList: ImmutableList<BannerData> = persistentListOf(),
+    val newSongData: ImmutableList<NewSongItemData> = persistentListOf(),
+    val chipContentList: ImmutableList<String> = persistentListOf(),
+    // 멜론 차트 데이터
+)
