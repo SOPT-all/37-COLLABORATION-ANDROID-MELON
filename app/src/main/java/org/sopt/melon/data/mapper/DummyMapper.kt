78e3fe88a119cd73fc969a4795875be46460dd61
seungjun <jjw5741@naver.com>
2025-11-24 23:37:43 +0900

    [fix/#23] dummy mapper 수정

--- app/src/main/java/org/sopt/melon/data/mapper/DummyMapper.kt
+++ app/src/main/java/org/sopt/melon/data/mapper/DummyMapper.kt
@@ -1,3 +1,10 @@
 package org.sopt.melon.data.mapper
 
+import org.sopt.melon.data.model.Dummy
+import org.sopt.melon.data.remote.dto.MusicDto
+
 // TODO: 각자 model 에 맞는 mapper 구현
+fun MusicDto.toModel() =
+    Dummy(
+        dummy = title,
+    )
--- app/src/main/java/org/sopt/melon/data/remote/service/DummyService.kt
+++ app/src/main/java/org/sopt/melon/data/remote/service/DummyService.kt
@@ -1,9 +1,9 @@
 package org.sopt.melon.data.remote.service
 
-import org.sopt.melon.data.remote.dto.DummyResponseDto
+import org.sopt.melon.data.remote.dto.MusicDto
 import retrofit2.http.GET
 
 interface DummyService {
     @GET("/api/v1/dummy")
-    suspend fun getDummy(): DummyResponseDto
+    suspend fun getDummy(): MusicDto
 }
