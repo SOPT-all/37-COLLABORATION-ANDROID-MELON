c1b2dc30ccde4b2df60fefe2c802de689ed2259f
dodo <eg4rang@naver.com>
2025-11-24 20:57:13 +0900

    [feat/#18] 더미데이터 연동

--- app/src/main/java/org/sopt/melon/presentation/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/melon/presentation/home/HomeViewModel.kt
@@ -5,6 +5,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import org.sopt.melon.presentation.home.model.BannerData
+import org.sopt.melon.presentation.home.model.PreferenceSongCardData
 import org.sopt.melon.presentation.home.type.NewSongFilter
 import javax.inject.Inject
 
@@ -12,6 +14,37 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState = _uiState.asStateFlow()
+
+    init {
+        getUserName()
+        getBannerList()
+        getPreferenceSong()
+    }
+
+    fun getUserName() {
+        _uiState.update { currentState ->
+            currentState.copy(
+                userName = "닉네임",
+            )
+        }
+    }
+
+
+    fun getBannerList() {
+        _uiState.update { currentState ->
+            currentState.copy(
+                bannerList = BannerData.dummyBannerData,
+            )
+        }
+    }
+
+    fun getPreferenceSong() {
+        _uiState.update { currentState ->
+            currentState.copy(
+                preferenceSong = PreferenceSongCardData.dummyPreferenceSongCardData,
+            )
+        }
+    }
 
     fun onNewSongFilterClick(newSongFilter: NewSongFilter) {
         _uiState.update { currentState ->
--- app/src/main/java/org/sopt/melon/presentation/home/model/PreferenceSongCardData.kt
+++ app/src/main/java/org/sopt/melon/presentation/home/model/PreferenceSongCardData.kt
@@ -1,8 +1,19 @@
 package org.sopt.melon.presentation.home.model
 
+import org.sopt.melon.R
+
 data class PreferenceSongCardData(
     val image: Int,
     val description: String,
     val songTitle: String,
     val singer: String,
-)
+) {
+    companion object {
+        val dummyPreferenceSongCardData = PreferenceSongCardData(
+            image = R.drawable.img_preference_95,
+            description = "내 취향 기반 추천곡",
+            songTitle = "The Day",
+            singer = "DAY6(데이식스)"
+        )
+    }
+}
