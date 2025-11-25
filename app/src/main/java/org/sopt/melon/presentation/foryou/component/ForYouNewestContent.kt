6653c9456a594d0cb244af9dd2c584addcafed65
seungjun <jjw5741@naver.com>
2025-11-25 10:52:24 +0900

    [refactor/#16] 컴포넌트 분리 및 패딩 수정

--- app/src/main/java/org/sopt/melon/presentation/foryou/component/ForYouNewestContent.kt
+++ app/src/main/java/org/sopt/melon/presentation/foryou/component/ForYouNewestContent.kt
@@ -1,12 +1,9 @@
 package org.sopt.melon.presentation.foryou.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -19,7 +16,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -27,11 +23,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import kotlinx.collections.immutable.persistentListOf
-import org.sopt.melon.R.drawable.img_newest_album
 import org.sopt.melon.R.drawable.img_newest_album_background
 import org.sopt.melon.R.drawable.img_newest_title_album
 import org.sopt.melon.R.string.foryou_newest_album_title
-import org.sopt.melon.R.string.foryou_newest_chip_title
 import org.sopt.melon.core.designsystem.theme.MELONTheme
 import org.sopt.melon.presentation.foryou.model.NewestAlbumData
 import org.sopt.melon.presentation.foryou.model.NewestMusicItemData
@@ -41,8 +35,6 @@ fun ForYouNewestContent(
     newestAlbum: NewestAlbumData,
     modifier: Modifier = Modifier,
 ) {
-    val colors = MELONTheme.colors
-
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -53,89 +45,73 @@ fun ForYouNewestContent(
             color = MELONTheme.colors.gray200,
         )
 
-        Box(
-            modifier = Modifier,
-        ) {
+        Box {
             AsyncImage(
                 model = newestAlbum.imageUrl,
                 contentDescription = null,
                 error = painterResource(img_newest_album_background),
-                contentScale = ContentScale.FillBounds,
-                modifier =
-                    Modifier
-                        .aspectRatio(320 / 448f)
-                        .clip(RoundedCornerShape(8.dp)),
-            )
-
-            Column(
                 modifier =
                     Modifier
                         .fillMaxWidth()
+                        .clip(RoundedCornerShape(8.dp)),
+            )
+
+            Box(
+                modifier =
+                    Modifier
                         .padding(
                             vertical = 18.dp,
                             horizontal = 12.dp,
                         ),
             ) {
-                Text(
-                    text = newestAlbum.title,
-                    style = MELONTheme.typography.heading.b_20,
-                    color = MELONTheme.colors.white,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                )
-
-                AsyncImage(
-                    model = newestAlbum.imageUrl,
-                    contentDescription = null,
-                    error = painterResource(img_newest_title_album),
+                Column(
                     modifier =
                         Modifier
-                            .padding(top = 9.dp)
-                            .size(160.dp)
-                            .clip(RoundedCornerShape(8.dp))
-                            .align(Alignment.CenterHorizontally),
-                )
-            }
-
-            Box(
-                modifier =
-                    Modifier
-                        .matchParentSize()
-                        .drawWithCache {
-                            val inset = 0.5.dp.toPx()
-                            val radius = 8.dp.toPx()
-
-                            onDrawBehind {
-                                drawRoundRect(
-                                    brush = colors.gradient4,
-                                    size =
-                                        Size(
-                                            width = size.width - inset * 2,
-                                            height = size.height - inset * 2,
-                                        ),
-                                    cornerRadius = CornerRadius(radius, radius),
-                                )
-                            }
-                        },
-            )
-
-            Column(
-                modifier =
-                    Modifier
-                        .padding(
-                            top = 226.dp,
-                            start = 12.dp,
-                            end = 12.dp,
-                        ),
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-            ) {
-                newestAlbum.newestMusicList.forEachIndexed { idx, music ->
-                    ForYouNestAlbumItem(
-                        albumUrl = music.imageUrl,
-                        title = music.title,
-                        artistName = music.artistName,
-                        isTitle = idx == 0,
+                            .fillMaxWidth(),
+                ) {
+                    Text(
+                        text = newestAlbum.title,
+                        style = MELONTheme.typography.heading.b_20,
+                        color = MELONTheme.colors.white,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
                     )
+
+                    AsyncImage(
+                        model = newestAlbum.imageUrl,
+                        contentDescription = null,
+                        error = painterResource(img_newest_title_album),
+                        modifier =
+                            Modifier
+                                .padding(top = 9.dp)
+                                .size(160.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                                .align(Alignment.CenterHorizontally),
+                    )
+                }
+
+                GradientBox(
+                    modifier =
+                        Modifier
+                            .matchParentSize(),
+                )
+
+                Column(
+                    modifier =
+                        Modifier
+                            .padding(
+                                top = 208.dp,
+                            ),
+                    verticalArrangement = Arrangement.spacedBy(10.dp),
+                ) {
+                    newestAlbum.newestMusicList.forEachIndexed { idx, music ->
+                        ForYouNewestAlbumItem(
+                            albumUrl = music.imageUrl,
+                            title = music.title,
+                            artistName = music.artistName,
+                            isTitle = idx == 0,
+                        )
+                    }
                 }
             }
         }
@@ -143,86 +119,30 @@ fun ForYouNewestContent(
 }
 
 @Composable
-private fun ForYouNestAlbumItem(
-    albumUrl: String,
-    title: String,
-    artistName: String,
-    isTitle: Boolean,
+private fun GradientBox(
     modifier: Modifier = Modifier,
 ) {
-    val (titleColor, artistColor) =
-        if (isTitle) {
-            MELONTheme.colors.primary to MELONTheme.colors.primary
-        } else {
-            MELONTheme.colors.white to MELONTheme.colors.opacity1
-        }
-
-    val titleTypo = if (isTitle) MELONTheme.typography.body.b_14 else MELONTheme.typography.body.r_14
-
-    Row(
+    val colors = MELONTheme.colors
+    Box(
         modifier =
             modifier
-                .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        AsyncImage(
-            model = albumUrl,
-            contentDescription = null,
-            error = painterResource(img_newest_album),
-            modifier =
-                Modifier
-                    .size(28.dp)
-                    .clip(RoundedCornerShape(4.dp))
-                    .border(
-                        width = 0.5.dp,
-                        brush = MELONTheme.colors.gradientVerticalLine,
-                        shape = RoundedCornerShape(4.dp),
-                    ),
-        )
+                .drawWithCache {
+                    val inset = 0.5.dp.toPx()
+                    val radius = 8.dp.toPx()
 
-        Row(
-            modifier =
-                Modifier
-                    .weight(1f),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
-            if (isTitle) {
-                Text(
-                    text = stringResource(foryou_newest_chip_title),
-                    style = MELONTheme.typography.caption.sb_8,
-                    color = MELONTheme.colors.primary,
-                    modifier =
-                        Modifier
-                            .border(
-                                width = 1.dp,
-                                color = MELONTheme.colors.primary,
-                                shape = RoundedCornerShape(8.dp),
-                            ).padding(
-                                vertical = 1.dp,
-                                horizontal = 3.5.dp,
-                            ),
-                )
-            }
-
-            Text(
-                text = title,
-                style = titleTypo,
-                color = titleColor,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-            )
-
-            Text(
-                text = artistName,
-                style = MELONTheme.typography.body.r_14,
-                color = artistColor,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-            )
-        }
-    }
+                    onDrawBehind {
+                        drawRoundRect(
+                            brush = colors.gradient4,
+                            size =
+                                Size(
+                                    width = size.width - inset * 2,
+                                    height = size.height - inset * 2,
+                                ),
+                            cornerRadius = CornerRadius(radius, radius),
+                        )
+                    }
+                },
+    )
 }
 
 @Preview(showBackground = true)
