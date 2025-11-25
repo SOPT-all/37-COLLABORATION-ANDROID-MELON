a367beb3a40d385955a70a708ce9ad69f73442ab
seungjun <jjw5741@naver.com>
2025-11-25 15:33:24 +0900

    [feat/#25] Album Service Module 추가

--- app/src/main/java/org/sopt/melon/data/remote/di/ServiceModule.kt
+++ app/src/main/java/org/sopt/melon/data/remote/di/ServiceModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.sopt.melon.data.remote.service.AlbumService
 import org.sopt.melon.data.remote.service.MusicService
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -13,5 +14,11 @@ import javax.inject.Singleton
 object ServiceModule {
     @Provides
     @Singleton
-    fun providesMusicService(retrofit: Retrofit): MusicService = retrofit.create(MusicService::class.java)
+    fun providesMusicService(retrofit: Retrofit): MusicService =
+        retrofit.create(MusicService::class.java)
+
+    @Provides
+    @Singleton
+    fun providesAlbumService(retrofit: Retrofit): AlbumService =
+        retrofit.create(AlbumService::class.java)
 }
