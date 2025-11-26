10540f22639c8cde8247290479dbedaddf1174d3
dodo <eg4rang@naver.com>
2025-11-26 23:05:30 +0900

    [feat/#27] Music Service 구현 - 체리픽 누락으로 코드 재작성

--- app/src/main/java/org/sopt/melon/data/remote/service/MusicService.kt
+++ app/src/main/java/org/sopt/melon/data/remote/service/MusicService.kt
@@ -3,8 +3,20 @@ package org.sopt.melon.data.remote.service
 import org.sopt.melon.core.network.model.BaseResponse
 import org.sopt.melon.data.remote.dto.MusicDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MusicService {
     @GET("music/custom-recommendation")
     suspend fun getCustomMusic(): BaseResponse<List<MusicDto>>
+
+    @GET("music/popular")
+    suspend fun getPopularMusicList(): BaseResponse<List<MusicDto>>
+
+    @GET("music/newest")
+    suspend fun getNewestMusicList(
+        @Query("category") category: String,
+    ): BaseResponse<List<MusicDto>>
+
+    @GET("music/chart")
+    suspend fun getChartMusicList(): BaseResponse<List<MusicDto>>
 }
