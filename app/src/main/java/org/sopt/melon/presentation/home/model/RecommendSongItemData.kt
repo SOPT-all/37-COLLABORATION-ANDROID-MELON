71115a6cd6734414141cfeca6d4ae1cb737f2d65
dodo <eg4rang@naver.com>
2025-11-26 17:56:40 +0900

    [feat/#25] UI Model 매퍼 구현

--- app/src/main/java/org/sopt/melon/presentation/home/model/RecommendSongItemData.kt
+++ app/src/main/java/org/sopt/melon/presentation/home/model/RecommendSongItemData.kt
@@ -1,6 +1,22 @@
 package org.sopt.melon.presentation.home.model
 
+import org.sopt.melon.data.model.HomeMusic
+
 class RecommendSongItemData(
     val imageUrl: String,
     val description: String,
 )
+
+fun HomeMusic.toRecommendSongItemData() {
+    val description = when (index) {
+        0 -> "내가 아끼는 최애곡 모음"
+        1 -> "요즘 듣던 노래 이어듣기"
+        2 -> "매일 찾아듣는 음악"
+        else -> ""
+    }
+
+    RecommendSongItemData(
+        imageUrl = imageUrl,
+        description = description,
+    )
+}
