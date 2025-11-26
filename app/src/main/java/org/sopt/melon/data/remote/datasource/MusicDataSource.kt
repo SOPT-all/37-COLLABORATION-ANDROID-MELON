dd15cb8252c44612cf2e73f09d0a72f7c1e8b741
dodo <eg4rang@naver.com>
2025-11-26 17:54:33 +0900

    [feat/#27] Music DataSource 구현
    # Conflicts:
    #	app/src/main/java/org/sopt/melon/data/remote/datasource/MusicDataSource.kt
    #	app/src/main/java/org/sopt/melon/data/remote/datasourceimpl/MusicDataSourceImpl.kt

--- app/src/main/java/org/sopt/melon/data/remote/datasource/MusicDataSource.kt
+++ app/src/main/java/org/sopt/melon/data/remote/datasource/MusicDataSource.kt
@@ -5,4 +5,12 @@ import org.sopt.melon.data.remote.dto.MusicDto
 
 interface MusicDataSource {
     suspend fun getCustomMusic(): BaseResponse<List<MusicDto>>
+
+    suspend fun getPopularMusicList(): BaseResponse<List<MusicDto>>
+
+    suspend fun getNewestMusicList(
+        category: String,
+    ): BaseResponse<List<MusicDto>>
+
+    suspend fun getChartMusicList(): BaseResponse<List<MusicDto>>
 }
