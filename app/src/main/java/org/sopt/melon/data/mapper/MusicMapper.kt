e5e1dbb0e9df6d7541e06d53592da01c839d2fc6
dodo <eg4rang@naver.com>
2025-11-26 17:54:45 +0900

    [feat/#27] Music Repository 구현

--- app/src/main/java/org/sopt/melon/data/mapper/MusicMapper.kt
+++ app/src/main/java/org/sopt/melon/data/mapper/MusicMapper.kt
@@ -1,10 +1,21 @@
 package org.sopt.melon.data.mapper
 
 import org.sopt.melon.data.model.ForYouMusic
+import org.sopt.melon.data.model.HomeMusic
 import org.sopt.melon.data.remote.dto.MusicDto
 
 fun MusicDto.toForYouMusicModel() =
     ForYouMusic(
+        title = title,
+        artistName = artistName,
+        imageUrl = imageUrl,
+    )
+
+fun MusicDto.toHomeMusicModel(
+    index: Int,
+) =
+    HomeMusic(
+        index = index,
         title = title,
         artistName = artistName,
         imageUrl = imageUrl,
