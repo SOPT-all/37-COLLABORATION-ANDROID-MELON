57328431e653ecc5b053780042c797c5bf0b4228
seungjun <jjw5741@naver.com>
2025-11-27 21:01:54 +0900

    [fix/#28] mixup 내부 패딩 수정

--- app/src/main/java/org/sopt/melon/presentation/mixup/MixUpScreen.kt
+++ app/src/main/java/org/sopt/melon/presentation/mixup/MixUpScreen.kt
@@ -6,9 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -117,7 +117,7 @@ private fun MixUpScreen(
                 Modifier
                     .align(Alignment.BottomCenter)
                     .background(color = defaultMelonColors.background2)
-                    .systemBarsPadding(),
+                    .navigationBarsPadding(),
         )
     }
 }
